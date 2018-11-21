c9a00d04122600e1688beca3d91c6ed8bf4a5fa6
kwu <kwu@riotgames.com>
2018-11-21 12:55:15 -0800

    Reworked create todo event

--- src/redux/index.js
+++ src/redux/index.js
@@ -29,11 +29,18 @@ const handleActions =
 
 // Actions
 
-export const createTodo = createAction('CREATE_TODO', (name, points) => ({
-  id: uuidv4(),
-  name,
-  points,
-}))
+export const createTodo = createAction('CREATE_TODO', (payload) => {
+  const id = uuidv4()
+  const { name, points, listing = 'backlog' } = payload
+
+  return {
+    id,
+    name,
+    points,
+    listing,
+  }
+})
+
 export const deleteTodo = createAction('DELETE_TODO', (id) => ({ id }))
 export const appendEvent = createAction('APPEND_EVENT')
 
